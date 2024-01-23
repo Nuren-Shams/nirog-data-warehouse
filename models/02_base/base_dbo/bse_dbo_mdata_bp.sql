719c70848d071b7902b67513fa4b98f8f7693e64
Nuren Shams <Nuren.Shams.Nirog@Gmail.com>
2024-01-23 17:47:58 +0000

    Removed CTE from bse_dbo_mdata_bp

--- models/02_base/base_dbo/bse_dbo_mdata_bp.sql
+++ models/02_base/base_dbo/bse_dbo_mdata_bp.sql
@@ -5,25 +5,17 @@
     )
 -}}
 
-
-WITH mdata_bp AS (
-    SELECT
-        patientid AS patient_id,
-        CAST(ROUND(SAFE_CAST(bpdiastolic1 AS FLOAT64)) AS INT64) AS bp_diastolic_1,
-        CAST(ROUND(SAFE_CAST(bpdiastolic2 AS FLOAT64)) AS INT64) AS bp_diastolic_2,
-        CAST(ROUND(SAFE_CAST(bpsystolic1 AS FLOAT64)) AS INT64) AS bp_systolic_1,
-        CAST(ROUND(SAFE_CAST(bpsystolic2 AS FLOAT64)) AS INT64) AS bp_systolic_2,
-        PARSE_TIMESTAMP("%Y-%m-%d %H:%M:%S", collectiondate) AS collected_at,
-        PARSE_TIMESTAMP("%Y-%m-%d %H:%M:%S", createdate) AS created_at,
-        PARSE_TIMESTAMP("%Y-%m-%d %H:%M:%S", updatedate) AS updated_at,
-
-    FROM
-        {{ ref("lan_dbo_mdatabp") }}
-)
-
-SELECT *
+SELECT
+    patientid AS patient_id,
+    CAST(ROUND(SAFE_CAST(bpdiastolic1 AS FLOAT64)) AS INT64) AS bp_diastolic_1,
+    CAST(ROUND(SAFE_CAST(bpdiastolic2 AS FLOAT64)) AS INT64) AS bp_diastolic_2,
+    CAST(ROUND(SAFE_CAST(bpsystolic1 AS FLOAT64)) AS INT64) AS bp_systolic_1,
+    CAST(ROUND(SAFE_CAST(bpsystolic2 AS FLOAT64)) AS INT64) AS bp_systolic_2,
+    PARSE_TIMESTAMP("%Y-%m-%d %H:%M:%S", collectiondate) AS collected_at,
+    PARSE_TIMESTAMP("%Y-%m-%d %H:%M:%S", createdate) AS created_at,
+    PARSE_TIMESTAMP("%Y-%m-%d %H:%M:%S", updatedate) AS updated_at,
 
 FROM
-    mdata_bp
+    {{ ref("lan_dbo_mdatabp") }}
 
 QUALIFY ROW_NUMBER() OVER(PARTITION BY patient_id, DATE(collected_at) ORDER BY updated_at DESC) = 1
