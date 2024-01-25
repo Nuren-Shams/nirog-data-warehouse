93bd9e1a6ef02e959efceed2920fba6cf61ec56d
Nuren Shams <Nuren.Shams.Nirog@Gmail.com>
2024-01-25 19:54:45 +0000

    Modifed  rep_daywise_htn &  rep_daywise_dm

--- models/05_report/report/rep_daywise_dm.sql
+++ models/05_report/report/rep_daywise_dm.sql
@@ -1,11 +1,11 @@
 {{-
     config(
         materialized = "table",
-        tags = ["execute_daily", "report", "htn"]
+        tags = ["execute_daily", "report", "dm"]
     )
 -}}
 
-SELECT 
+SELECT
     mdata.patient_id
     , p.given_name
     , p.family_name
@@ -14,12 +14,21 @@ SELECT
     , p.id_type
     , mdata.collected_date
     , mdata.is_pregnant
+    , p.workplace_id
+    , p.workplace_name 
+    , p.workplace_branch_code
+    , p.district_name
     , mdata.fbg
     , mdata.rbg
+    , mdata.prescribed_rx
 
 FROM
     {{ ref("stg_cor_mdata_super_table") }} AS mdata
 
     LEFT JOIN {{ ref("stg_cor_patient_extended") }} AS p
-        ON 
+        ON
             mdata.patient_id = p.patient_id
+
+WHERE 
+    TRUE
+    AND fbg IS NOT NULL
--- models/05_report/report/rep_daywise_htn.sql
+++ models/05_report/report/rep_daywise_htn.sql
@@ -14,6 +14,10 @@ SELECT
     , p.id_type
     , mdata.collected_date
     , mdata.is_pregnant
+    , p.workplace_id
+    , p.workplace_name 
+    , p.workplace_branch_code
+    , p.district_name
     , COALESCE(mdata.bp_systolic_2, mdata.bp_systolic_1) AS bp_systolic
     , COALESCE(mdata.bp_diastolic_2, mdata.bp_diastolic_1) AS bp_diastolic
     , mdata.prescribed_rx
@@ -24,3 +28,7 @@ FROM
     LEFT JOIN {{ ref("stg_cor_patient_extended") }} AS p
         ON
             mdata.patient_id = p.patient_id
+
+WHERE 
+    TRUE
+    AND COALESCE(mdata.bp_systolic_2, mdata.bp_systolic_1) IS NOT NULL
