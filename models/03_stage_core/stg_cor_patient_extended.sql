12c5aec447b2114fd6b8d72cc12a9e0cde723b6b
Nuren Shams <Nuren.Shams.Nirog@Gmail.com>
2024-01-30 17:15:13 +0000

    Added bse_dbo_health_center & bse_dbo_barcode_formats

--- models/03_stage_core/stg_cor_patient_extended.sql
+++ models/03_stage_core/stg_cor_patient_extended.sql
@@ -13,7 +13,10 @@ SELECT
     , wp.district_id
     , wpb.workplace_branch_code
     , wpb.description AS workplace_branch_description
-    , ast.district_name
+    -- , ast.district_name
+    -- , ast.upazila_name
+    -- , ast.union_name
+
 FROM 
     {{ ref("bse_dbo_patient") }} AS p
 
@@ -25,6 +28,6 @@ FROM
         ON
             p.workplace_branch_id = wpb.workplace_branch_id
 
-    LEFT JOIN {{ ref("stg_cor_area_super_table") }} AS ast
-        ON
-            wp.district_id = ast.district_id_hash
+    -- LEFT JOIN {{ ref("stg_cor_area_super_table") }} AS ast
+    --     ON
+    --         wp.district_id = ast.district_id_hash
