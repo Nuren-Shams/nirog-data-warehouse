ec7458828cf42199080b9f14b60abbd097867f06
Nuren Shams <Nuren.Shams.Nirog@Gmail.com>
2024-02-11 14:28:31 +0000

    Refactored *_screened_* fields to *_diagnosed_* to fields. Removed the Cumulative fields.

--- models/05_report/rep_dm_info_period_wise.sql
+++ models/05_report/rep_dm_info_period_wise.sql
@@ -13,15 +13,10 @@ SELECT
     , union_name 
     , "" AS period
     , registered_patients
-    , dm_screened_patients
+    , dm_diagnosed_patients
     , non_dm_patients
     , medication_received_patients
     , lost_followup_patients
-    , cumulative_registered_patients
-    , cumulative_dm_screened_patients
-    , cumulative_non_dm_patients
-    , cumulative_medication_received_patients
-    , cumulative_lost_followup_patients
 
 FROM 
     {{ ref("stg_agg_dm_info_daily") }}
