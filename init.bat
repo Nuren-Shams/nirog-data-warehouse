63cce20794c55cbdab600e3ddbc9199751eaca5d
Nuren Shams <NurenShams@Pathao.com>
2024-04-07 15:49:50 +0600

    Added the initializer script

---
+++ init.bat
@@ -0,0 +1,11 @@
+CALL conda clean --all --verbose --yes
+CALL conda env remove --name nirog-data-warehouse-environment --verbose --yes
+CALL conda create --name nirog-data-warehouse-environment python=3.9 --verbose --yes
+CALL conda activate nirog-data-warehouse-environment
+python -m pip install pip --upgrade --no-cache-dir --verbose
+pip install -r %~dp0\requirements.txt --no-cache-dir --verbose
+cd %~dp0\core
+dbt deps
+dbt debug
+CALL conda deactivate
+PAUSE
