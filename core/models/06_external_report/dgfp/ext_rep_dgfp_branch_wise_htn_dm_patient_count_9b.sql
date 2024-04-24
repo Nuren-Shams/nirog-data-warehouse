281bc70f94dd4ea5e402f78b58d4ba6cdf187096
Nuren Shams <NurenShams@Pathao.com>
2024-04-24 23:43:32 +0600

    Added the data models for the DGFP Report

---
+++ core/models/06_external_report/dgfp/ext_rep_dgfp_branch_wise_htn_dm_patient_count_9b.sql
@@ -0,0 +1,29 @@
+{{-
+    config(
+        materialized = "table",
+        tags = ["execute_daily", "external", "report", "htn_dm", "branch_wise"]
+    )
+-}}
+
+SELECT
+    `District_Name`,
+    `Upazila_Name`,
+    `Union_Name`,
+    `Health_Center_Name`,
+    `Barcode_Prefix`,
+    `Period_Start_Date`,
+    `Registered_Patients`,
+    `HTN_Screened_Patients`,
+    `HTN_Screened_Male_Patients`,
+    `HTN_Screened_Female_Patients`,
+    `HTN_Diagnosed_Patients`,
+    `DM_Screened_Patients`,
+    `DM_Screened_Male_Patients`,
+    `DM_Screened_Female_Patients`,
+    `DM_Diagnosed_Patients`
+
+FROM
+    {{ ref('rep_branch_wise_htn_dm_patient_count_9b') }}
+
+WHERE TRUE
+    {{ mcr_dgfp_health_center_filter("`Health_Center_Name`") }}
